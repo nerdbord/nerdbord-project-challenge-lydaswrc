b90b34f0d4084fb96619184f0c34687c913a29bb
transcendence12 <gosiek.krawczuk@gmail.com>
2024-07-12 14:39:03 +0200

    feat: collect emails, add subscriber to the sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -18,6 +18,7 @@ import { pageStructure, singletonPlugin } from "@/sanity/plugins/settings";
 import { assistWithPresets } from "@/sanity/plugins/assist";
 import author from "@/sanity/schemas/documents/author";
 import post from "@/sanity/schemas/documents/post";
+import subscriber from "./sanity/schemas/documents/subscriber";
 import settings from "@/sanity/schemas/singletons/settings";
 import { resolveHref } from "@/sanity/lib/utils";
 
@@ -35,6 +36,7 @@ export default defineConfig({
       // Singletons
       settings,
       // Documents
+      subscriber,
       post,
       author,
     ],
