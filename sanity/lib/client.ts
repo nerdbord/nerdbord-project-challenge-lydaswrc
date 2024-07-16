ca99dd979ced0d63dd6faab6dcc15b84a3c38025
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-07-16 17:46:31 +0200

    feat: collect emails - fix: merge main

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -10,7 +10,6 @@ export const client = createClient({
   apiVersion,
   useCdn: true,
   perspective: "published",
-  token: process.env.SANITY_API_EDIT_TOKEN,
   stega: {
     studioUrl,
     logger: console,
